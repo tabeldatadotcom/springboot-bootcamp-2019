e339c48461f0f1e5a678b34dd60e0a3f851e085e
dimmaryanto93 <software.dimas_m93@icloud.com>
2019-02-27 15:56:04 +0700

    init project springboot

--- src/main/java/com/tabeldata/bootcamp/belajarspringboot/BelajarSpringbootApplication.java
+++ src/main/java/com/tabeldata/bootcamp/belajarspringboot/BelajarSpringbootApplication.java
@@ -2,24 +2,12 @@ package com.tabeldata.bootcamp.belajarspringboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
-
-import com.tabeldata.bootcamp.belajarspringboot.beans.Kelas;
-import com.tabeldata.bootcamp.belajarspringboot.beans.Mahasiswa;
 
 @SpringBootApplication
 public class BelajarSpringbootApplication {
 	
-
     public static void main(String[] args) {
-        ApplicationContext context = SpringApplication.run(BelajarSpringbootApplication.class, args);
-        
-        Kelas si = (Kelas) context.getBean("kelas");
-        System.out.println(si.getNama());
-        
-        Mahasiswa dimas = context.getBean("gufron",Mahasiswa.class);
-        System.out.println(dimas.toString());
+        SpringApplication.run(BelajarSpringbootApplication.class, args);
     }
 
 }
