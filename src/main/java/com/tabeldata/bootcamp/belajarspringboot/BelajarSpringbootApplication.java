ad92ed1d3203eac089cebee1977c2eac482faaf8
dimmaryanto93 <software.dimas_m93@icloud.com>
2019-02-27 11:40:53 +0700

    perbedaan singleton dan prototype

--- src/main/java/com/tabeldata/bootcamp/belajarspringboot/BelajarSpringbootApplication.java
+++ src/main/java/com/tabeldata/bootcamp/belajarspringboot/BelajarSpringbootApplication.java
@@ -6,12 +6,52 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class BelajarSpringbootApplication {
 
     public static void main(String[] args) {
-       ApplicationContext context = new ClassPathXmlApplicationContext("spring-container.xml");
-       Mahasiswa dimas =(Mahasiswa) context.getBean("mahasiswa");    
-       
-       Kelas si =(Kelas) context.getBean("kelas");
-      
-       System.out.println("nama: "+ dimas.getNama() +" semester: "+dimas.getSemester()+" kelas: "+ si.getNama());
+        ApplicationContext context = new ClassPathXmlApplicationContext("spring-container.xml");
+//        Mahasiswa dimas = (Mahasiswa) context.getBean("mahasiswa");
+//
+//        Kelas si = (Kelas) context.getBean("kelas");
+//
+//        System.out.println("nama: " + dimas.getNama() + " semester: " + dimas.getSemester() + " kelas: " + si.getNama());
+
+        System.out.println("--- singleton ---");
+        RandomString rs = context.getBean("randomString", RandomString.class);
+        System.out.println(rs.getValue());
+
+        rs = context.getBean("randomString", RandomString.class);
+        System.out.println(rs.getValue());
+
+        rs = context.getBean("randomString", RandomString.class);
+        System.out.println(rs.getValue());
+
+        rs = context.getBean("randomString", RandomString.class);
+        System.out.println(rs.getValue());
+
+        rs = context.getBean("randomString", RandomString.class);
+        System.out.println(rs.getValue());
+
+        rs = context.getBean("randomString", RandomString.class);
+        System.out.println(rs.getValue());
+
+        System.out.println("---prototype ---");
+
+        RandomStringPrototype proto = context.getBean(RandomStringPrototype.class);
+        System.out.println(proto.getValue());
+        
+        proto = context.getBean(RandomStringPrototype.class);
+        System.out.println(proto.getValue());
+        
+        proto = context.getBean(RandomStringPrototype.class);
+        System.out.println(proto.getValue());
+        
+        proto = context.getBean(RandomStringPrototype.class);
+        System.out.println(proto.getValue());
+        
+        proto = context.getBean(RandomStringPrototype.class);
+        System.out.println(proto.getValue());
+        
+        proto = context.getBean(RandomStringPrototype.class);
+        System.out.println(proto.getValue());
+
     }
 
 }
