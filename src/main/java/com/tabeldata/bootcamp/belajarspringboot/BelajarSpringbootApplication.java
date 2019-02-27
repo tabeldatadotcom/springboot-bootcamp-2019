d4d876aab1070595513348d253c4b0597da5efb9
dimmaryanto93 <software.dimas_m93@icloud.com>
2019-02-27 15:44:19 +0700

    migration from spring framework to springboot

--- src/main/java/com/tabeldata/bootcamp/belajarspringboot/BelajarSpringbootApplication.java
+++ src/main/java/com/tabeldata/bootcamp/belajarspringboot/BelajarSpringbootApplication.java
@@ -1,16 +1,38 @@
 package com.tabeldata.bootcamp.belajarspringboot;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
 
 import com.tabeldata.bootcamp.belajarspringboot.beans.Kelas;
 import com.tabeldata.bootcamp.belajarspringboot.beans.Mahasiswa;
-import com.tabeldata.bootcamp.belajarspringboot.config.SpringConfig;
 
+@SpringBootApplication
 public class BelajarSpringbootApplication {
+	
+	@Bean
+	public String nama1() {
+		return "Configurasion dari bean";
+	}
+	
+	@Bean
+	public String nama2() {
+		return "Configurasion dari bean 2";
+	}
+	
+	@Bean
+	public Mahasiswa dimas() {
+		return new Mahasiswa("Dimas Maryanto", 7);
+	}
+	
+	@Bean
+	public Mahasiswa gufron() {
+		return new Mahasiswa("Gufron", 7);
+	}
 
     public static void main(String[] args) {
-        ApplicationContext context = new AnnotationConfigApplicationContext(SpringConfig.class);
+        ApplicationContext context = SpringApplication.run(BelajarSpringbootApplication.class, args);
         
         Kelas si = (Kelas) context.getBean("kelas");
         System.out.println(si.getNama());
