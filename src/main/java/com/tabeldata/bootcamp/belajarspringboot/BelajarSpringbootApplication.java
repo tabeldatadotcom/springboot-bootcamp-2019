a805298495ff310f09f59bebfebfeafef1f3eae5
dimmaryanto93 <software.dimas_m93@icloud.com>
2019-02-27 11:02:58 +0700

    penamaan beans

--- src/main/java/com/tabeldata/bootcamp/belajarspringboot/BelajarSpringbootApplication.java
+++ src/main/java/com/tabeldata/bootcamp/belajarspringboot/BelajarSpringbootApplication.java
@@ -7,9 +7,11 @@ public class BelajarSpringbootApplication {
 
     public static void main(String[] args) {
        ApplicationContext context = new ClassPathXmlApplicationContext("spring-container.xml");
-       Mahasiswa dimas =(Mahasiswa) context.getBean("mahasiswa");       
+       Mahasiswa dimas =(Mahasiswa) context.getBean("mahasiswa");    
+       
+       Kelas si =(Kelas) context.getBean("kelas");
       
-       System.out.println("nama: "+ dimas.getNama() +" semester: "+dimas.getSemester()+" kelas: "+ dimas.getKelas().getNama());
+       System.out.println("nama: "+ dimas.getNama() +" semester: "+dimas.getSemester()+" kelas: "+ si.getNama());
     }
 
 }
