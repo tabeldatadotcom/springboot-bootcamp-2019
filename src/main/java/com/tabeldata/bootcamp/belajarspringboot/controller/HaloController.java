9b3a8b39e7d84ad5cdfe219547ce4c18acdd26cb
dimmaryanto93 <software.dimas_m93@icloud.com>
2019-03-04 13:58:16 +0700

    perbedaan antara post dan get request method di web

--- src/main/java/com/tabeldata/bootcamp/belajarspringboot/controller/HaloController.java
+++ src/main/java/com/tabeldata/bootcamp/belajarspringboot/controller/HaloController.java
@@ -2,13 +2,27 @@ package com.tabeldata.bootcamp.belajarspringboot.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HaloController {
-	
+
 	@GetMapping("/halo")
-	public String halo() {
+	public String halo(
+			@RequestParam(name = "nama", required = false) String nama) {
+		System.out.println("nama saya : " + nama);
 		return "index";
 	}
+	
+	@GetMapping("/form")
+	public String form() {
+		return "form";
+	}
 
+	@PostMapping("/halo")
+	public String haloPost(@RequestParam(name = "nama", required = false) String nama) {
+		System.out.println("nama saya : " + nama);
+		return "index";
+	}
 }
