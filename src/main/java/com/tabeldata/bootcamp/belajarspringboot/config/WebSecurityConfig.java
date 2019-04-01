13f92377796d12ce47e4822f21db456305828f9d
dimmaryanto93 <software.dimas_m93@icloud.com>
2019-04-01 11:20:59 +0700

    membuat custome user by beans di spring security

---
+++ src/main/java/com/tabeldata/bootcamp/belajarspringboot/config/WebSecurityConfig.java
@@ -0,0 +1,38 @@
+package com.tabeldata.bootcamp.belajarspringboot.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder(11);
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(PasswordEncoder encoder) {
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+        manager.createUser(
+                User.withUsername("dimas")
+                        .password(encoder.encode("admin"))
+                        .roles("USER", "ADMIN", "OPERATOR")
+                        .build());
+        manager.createUser(User.withUsername("himawan")
+                .password(encoder.encode("admin"))
+                .roles("USER", "ADMIN", "OPERATOR")
+                .build());
+        return manager;
+    }
+}
